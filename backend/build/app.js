ca4f6d00dc8e2fe2830b2f3fa879741408f6222c
burakdiker <burak.diker@ug.bilkent.edu.tr>
2024-02-13 06:26:36 +0300

    Add  build CORS middleware to app.js

--- backend/build/app.js
+++ backend/build/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 const database_connection_1 = require("./database-connection");
 const errorHandler_1 = require("./middlewares/errorHandler");
 const users_1 = __importDefault(require("./routes/users"));
@@ -19,6 +20,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)('dev'));
 app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
 // routes
 app.use('/api/users', users_1.default);
 app.use('/api/login', login_1.default);
